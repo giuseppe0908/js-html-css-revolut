622d14cf954c165f6dbecc85f2c602b5625fbbeb
giuseppe <placidagiuseppe@gmail.com>
2021-03-12 17:56:54 +0100

    ....

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+//utilizzo la funzione .ready per essere sicuro di far eseguire il codice dopo il caricamento del DOM
+// $(document).ready(function(){
+//   hover() ;
+//
+// })
+
+// function hover() {
+//   var feature = $('#flex-end li .feature');
+//   feature.mouseenter() = feature.css('display','block');
+//   console.log(feature);
+// }
