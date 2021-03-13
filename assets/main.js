8a2751177af772af7dc58751e47cd1a7b55c5459
giuseppe <placidagiuseppe@gmail.com>
2021-03-13 17:07:17 +0100

    esercizio terminato

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,32 @@
-//utilizzo la funzione .ready per essere sicuro di far eseguire il codice dopo il caricamento del DOM
-// $(document).ready(function(){
-//   hover() ;
-//
-// })
+// utilizzo la funzione .ready per essere sicuro di far eseguire il codice dopo il caricamento del DOM
+$(document).ready(function(){
+  hover() ;
 
-// function hover() {
-//   var feature = $('#flex-end li .feature');
-//   feature.mouseenter() = feature.css('display','block');
-//   console.log(feature);
-// }
+})
+
+function hover() {
+  var feature = $('.position-relative');
+  var element_nav = $('nav ul li a');
+  var lista = $('.dropdown ul li');
+    feature.mouseenter(function(){
+      feature = $(this).children('.dropdown').removeClass('none').addClass('active');
+    })
+    feature.mouseleave(function(){
+      feature = $(this).children('.dropdown').removeClass('active').addClass('none');
+    })
+    lista.mouseenter(function(){
+      lista= $(this).children('a').css('color','black');
+
+    })
+    lista.mouseleave(function(){
+      lista = $(this).children('a').css('color','#7F8081');
+    })
+    element_nav.mouseenter(function(){
+      element_nav= $(this).css('color','black');
+
+    })
+    element_nav.mouseleave(function(){
+      element_nav = $(this).css('color','#7F8081');
+    })
+
+}
